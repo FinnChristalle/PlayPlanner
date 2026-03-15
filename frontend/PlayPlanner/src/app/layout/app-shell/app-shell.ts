1eed50457851aaeced89d8127e8d3479c3efb4b4
Finn <finn.christalle@gmail.com>
2026-03-15 14:28:21 +0100

    Add responsive app shell structure with TailwindCSS integration

---
+++ frontend/PlayPlanner/src/app/layout/app-shell/app-shell.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { Header } from '../header/header';
+import { Sidebar } from '../sidebar/sidebar';
+
+@Component({
+  selector: 'app-app-shell',
+  imports: [
+	    NgIf,
+	    RouterOutlet,
+	    Header,
+	    Sidebar
+	  ],
+  templateUrl: './app-shell.html',
+  styleUrl: './app-shell.scss',
+})
+export class AppShell {
+  isSidebarOpen = false;
+
+  toggleSidebar(): void {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+
+  closeSidebar(): void {
+    this.isSidebarOpen = false;
+  }
+}
